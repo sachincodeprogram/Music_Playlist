c23c3db05ec0d6c6d7237c13c3ff96c75ff96113
sachincodeprogram <132731221+sachincodeprogram@users.noreply.github.com>
2023-08-23 14:21:18 +0530

    Update MusicPlayList_styledComponents.js

--- MusicPlayList_styledComponents.js
+++ MusicPlayList_styledComponents.js
@@ -1,76 +1,56 @@
 import styled from 'styled-components'
 
-export const BgContainer = styled.div`
-  background-color: #152850;
-  min-height: 100vh;
-`
-
-export const ArtistBg = styled.div`
-background-image: url('https://assets.ccbp.in/frontend/react-js/music-playlist/music-playlist-Edsheeran-bg.png')
-height: 450px;
-background-size: cover;
-display: flex;
-flex-direction: column;
-justify-content: flex-end;
-padding-left: 100px;
-`
-
-export const ArtistName = styled.h1`
-  font-family: 'Roboto';
-  color: #fff;
-  font-size: 42px;
-  font-weight: bold;
-`
-
-export const ArtistInfo = styled.p`
-  font-family: 'Roboto';
-  color: #fff;
-  font-size: 32px;
-  font-weight: 400;
-`
-
-export const PlayListContainer = styled.div`
+export const MusicListItem = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-left: 50px;
-  margin-right: 50px;
+  list-style-type: none;
+`
+export const TrackInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 500px;
 `
 
-export const PlayListHeading = styled.h1`
+export const MusicTrackImage = styled.img`
+  height: 90px;
+  width: 140px;
+  margin-right: 20px;
+`
+
+export const NameGenreContainer = styled.div``
+
+export const Name = styled.p`
   font-family: 'Roboto';
   color: #fff;
-  font-size: 32px;
-  font-weight: bold;
+  font-size: 18px;
+  font-weight: 500;
 `
 
-export const SearchInput = styled.div`
-  height: 30px;
-  width: 18%;
-  padding-left: 15px;
-  color: #cbd5e1;
-  border-radius: 4px;
-  border: 1px solid #fff;
-  background-color: transparent;
-  outline: none;
-  cursor: pointer;
+export const Genre = styled.p`
+  color: #3b82f6;
+  font-family: 'Roboto';
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 30px;
 `
 
-export const MusicList = styled.ul`
-  padding: 0;
-  margin-left: 50px;
-  margin-right: 50px;
-`
-
-export const EmptyContainer = styled.div`
+export const DurationDeleteContainer = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
 `
 
-export const NoSongsText = styled.p`
+export const Duration = styled.p`
   font-family: 'Roboto';
   color: #fff;
-  font-size: 32px;
+  font-size: 20px;
   font-weight: bold;
+  margin-right: 50px;
+`
+export const DeleteButton = styled.button`
+  background-color: transparent;
+  outline: none;
+  cursor: pointer;
+  border: none;
+  color: #fff;
 `
